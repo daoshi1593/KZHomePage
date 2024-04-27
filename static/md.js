1dc36f5d85a366928b29f4b362e804bcfb6cbe1e
daoshi1593 <144124386+daoshi1593@users.noreply.github.com>
2024-04-27 19:41:06 +0800

    v3

--- static/md.js
+++ static/md.js
@@ -24,133 +24,71 @@ async function updateBlog() {
       <a href="md-files/${title}.html">${title}</a>
       </dd>`;
     // 记载每一个md文件内容,为每一个创建一个html页面在md-files文件夹下
-    const htmlData = `<!DOCTYPE html>
-    <html>
-    <head>
-      <meta charset="utf-8">
-      <title>layout daoshi1593的个人主页 - Layui</title>
-      <meta name="renderer" content="webkit">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link href="//cdn.staticfile.org/layui/2.9.7/css/layui.css" rel="stylesheet">
-    </head>
-    <body>
-    <div class="layui-layout layui-layout-admin">
-      <div class="layui-header">
-        <div class="layui-logo layui-hide-xs layui-bg-black">layout demo</div>
-        <!-- 头部区域（可配合layui 已有的水平导航） -->
-        <ul class="layui-nav layui-layout-left">
-          <!-- 移动端显示 -->
-          <li class="layui-nav-item layui-show-xs-inline-block layui-hide-sm" lay-header-event="menuLeft">
-            <i class="layui-icon layui-icon-spread-left"></i>
-          </li>
-          <li class="layui-nav-item">
-              
-          <!-- 电脑端显示 -->
-          
-          </li>
-        </ul>
-        <ul class="layui-nav layui-layout-right">
-          <li class="layui-nav-item layui-hide layui-show-sm-inline-block">
-            <a href="javascript:;">
-              <img src="//unpkg.com/outeres@0.0.10/img/layui/icon-v2.png" class="layui-nav-img">
-              tester
-            </a>
-            <dl class="layui-nav-child">
-              <dd><a href="..\index.html">index</a></dd>
-              <dd><a href="..\cv.html">CV</a></dd>
-              <dd><a href="..\blog.html">Blog</a></dd>
-            </dl>
-          </li>
-          <li class="layui-nav-item" lay-header-event="menuRight" lay-unselect>
-            <a href="javascript:;">
-              <i class="layui-icon layui-icon-more-vertical"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="layui-side layui-bg-black">
-        <div class="layui-side-scroll">
-          <!-- 左侧导航区域（可配合layui已有的垂直导航） -->
-          <ul class="layui-nav layui-nav-tree" lay-filter="test">
-            <li class="layui-nav-item layui-nav-itemed">
-              <a class="" href="javascript:;">学习笔记</a>
-              <dl class="layui-nav-child">
-                <!-- start insert -->
-    
-                <!-- end insert -->
-              </dl>
-            </li>
-            <li class="layui-nav-item">
-              <a href="javascript:;">menu group 2</a>
-              <dl class="layui-nav-child">
-                <dd><a href="javascript:;">list 1</a></dd>
-                <dd><a href="javascript:;">list 2</a></dd>
-                <dd><a href="javascript:;">超链接</a></dd>
-              </dl>
-            </li>
-            <li class="layui-nav-item"><a href="javascript:;">click menu item</a></li>
-            <li class="layui-nav-item"><a href="javascript:;">the links</a></li>
-          </ul>
-        </div>
-      </div>
-      <div class="layui-body" style="background-color: rgb(227, 237, 205);overflow-y: auto;">
-        <!-- 内容主体区域 -->
-        <div style="padding: 15px;">
-          <blockquote class="layui-elem-quote layui-text" style="color: aqua;">
-            daoshi1593的个人主页
-          </blockquote>
-            <div class="layui-card-body">
-              <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <!-- 标题居中 -->
-                <title style="font-size: 100%;">${title} <br></title>
-                <link rel="stylesheet" href="..\static\styles.css">
-              </head>
-              <body>
-                <div>${marked(data)}</div>
-              </body>
-          </div>
-        </div>
-      </div>
-      <div class="layui-footer">
-        <!-- 底部固定区域 -->
-        底部固定区域
-      </div>
-    </div>
-     
-    <script src="//cdn.staticfile.org/layui/2.9.7/layui.js"></script>
-    <script>
-    //JS 
-    layui.use(['element', 'layer', 'util'], function(){
-      var element = layui.element;
-      var layer = layui.layer;
-      var util = layui.util;
-      var $ = layui.$;
+    const htmlData = `
+<!DOCTYPE html>
+<html lang="ch-cn">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Begin Jekyll SEO tag v2.8.0 -->
+<title>${title} | daoshi1593</title>
+<meta name="generator" content="Jekyll v3.9.5" />
+<meta property="og:title" content="${title}" />
+<meta property="og:locale" content="en_US" />
+<link rel="canonical" href="https://daoshi1593.github.io/md-files/${title}.html" />
+<meta property="og:url" content="https://daoshi1593.github.io/md-files/${title}.html" />
+<meta property="og:site_name" content="daoshi1593" />
+<meta property="og:type" content="website" />
+<meta name="twitter:card" content="summary" />
+<meta property="twitter:title" content="${title}" />
+<script type="application/ld+json">
+{"@context":"https://schema.org","@type":"WebPage","headline":"${title}","url":"https://daoshi1593.github.io/md-files/cs106b_ALL.html"}</script>
+<!-- End Jekyll SEO tag -->
+
+    <link rel="stylesheet" href="../static/blog.css">
+    <!-- start custom head snippets, customize with your own _includes/head-custom.html file -->
+
+<!-- Setup Google Analytics -->
+
+
+
+<!-- You can set your favicon here -->
+<!-- link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" -->
+
+<!-- end custom head snippets -->
+
+  </head>
+  <body>
+    <div class="container-lg px-3 my-5 markdown-body">
       
-      //头部事件
-      util.event('lay-header-event', {
-        menuLeft: function(othis){ // 左侧菜单事件
-          layer.msg('展开左侧菜单的操作', {icon: 0});
-        },
-        menuRight: function(){  // 右侧菜单事件
-          layer.open({
-            type: 1,
-            title: '更多',
-            content: '<div style="padding: 15px;">处理右侧面板的操作</div>',
-            area: ['260px', '100%'],
-            offset: 'rt', // 右上角
-            anim: 'slideLeft', // 从右侧抽屉滑出
-            shadeClose: true,
-            scrollbar: false
-          });
-        }
-      });
-    });
-    </script>
-    </body>
-    </html>`;
+      <h1><a href="https://daoshi1593.github.io/">daoshi1593</a></h1>
+      <div style=" display: flex;">
+        <h2>
+            <a href="https://daoshi1593.github.io/blog">Blog</a>
+        </h2>
+        <h2>
+            <a href="https://daoshi1593.github.io/cv">CV</a>
+        </h2>
+      </div>
+      
+
+      <body>
+        <div>${marked(data)}</div>
+      </body>
+
+      
+      <div class="footer border-top border-gray-light mt-5 pt-3 text-right text-gray">
+        This site is open source. <a href="https://github.com/daoshi1593/daoshi1593.github.io/edit/main/md-files/${title}.md">Improve this page</a>.
+      </div>
+      
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/anchor-js/4.1.0/anchor.min.js" integrity="sha256-lZaRhKri35AyJSypXXs4o6OPFTbTmUoltBbDCbdzegg=" crossorigin="anonymous"></script>
+    <script>anchors.add();</script>
+  </body>
+</html>
+    `;
     await writeFileAsync(join('../md-files', `${title}.html`), htmlData);
   }
 
